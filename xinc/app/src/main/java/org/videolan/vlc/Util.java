288e850b6206ae75c5fccfd2a2cd1386d475be90
macvisionjohn <john@macvision.cc>
2016-02-29 14:08:38 +0800

    048  1.0.53版本 稳定版（双路照片前备份）

--- xinc/app/src/main/java/org/videolan/vlc/Util.java
+++ xinc/app/src/main/java/org/videolan/vlc/Util.java
@@ -314,6 +314,7 @@ public class Util {
     public static boolean isJellyBeanOrLater()
     {
         return android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN;
+//        return false;
     }
 
     public static boolean hasExternalStorage() {
